e39cc498cf2278bbae5dd4585f1b43325dd41fc9
mubaraqhere <mubaraqhere@gmail.com>
2020-09-01 23:04:20 +0700

    reusable components (card from dgimonPage)

---
+++ client/src/components/Card.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { Link } from "react-router-dom";
+
+export default (props) => {
+    return (
+        <>
+            {/* {JSON.stringify(this.props.digimon)} */}
+            <div className="col-3 mb-3">
+                <div className="card" style={{width: "18rem"}}> 
+                    <img src={props.digimon.img} className="card-img-top" alt="pokemon"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.digimon.name}</h5>
+                        <p className="card-text">{props.digimon.level}</p>
+                        <Link to={`/detail/${props.digimon.name}`} className="btn btn-primary">Detail</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
