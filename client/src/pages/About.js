333c91a8902fdaace95d244a66da2a537493523e
mubaraqhere <mubaraqhere@gmail.com>
2020-09-01 23:03:15 +0700

    add About

---
+++ client/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+            <div className="mt-3">
+                <h1 className="">About</h1>
+                <p>Hello Everyone, This page is where the react.js lesson in Hacktiv8 is for displaying data from digimon 3th party API.
+                    <br/> 
+                    If the display has appeared, it is a sign that the lesson in this exercise has been passed. 
+                    <br/>   
+                    You can take a lesson from how I call digimon data both to display all the data and only based on the id. </p>
+            </div>
+        </>
+    )
+}
+
