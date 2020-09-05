96e53533801eda3831d3797416453f5ef7cda0d8
mubaraqhere <mubaraqhere@gmail.com>
2020-09-06 02:58:32 +0700

    Update: + refactor to middleware

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,32 +1,22 @@
-import React from 'react';
-import useFetcher from '../hooks/useFetcher'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
+import { getDetail } from '../store/actions/digimonAction'
 
 export default () => {
     const { name } = useParams();
-    const baseUrl = `https://digimon-api.vercel.app/api/digimon/name/${name}`
-    const {data:digimons, loading, error} = useFetcher(baseUrl)
+    const digimon = useSelector((state) => state.digimonReducer.digimon);
+    const dispatch = useDispatch();
 
-    if (loading) {
-        return (
-            <>
-                <div>
-                    <p>Waiting for mmoment gan...</p>
-                </div>
-            </>
+    useEffect(() => {
+        dispatch(getDetail(name))
+    }, [name, dispatch])
+
+    if (digimon === null) {
+        return(
+            <p>Loading guys....</p>
         )
     }
-
-    if (error) {
-        return (
-            <>
-                <div>
-                    <p>Sometimes When Wrong</p>
-                </div>
-            </>
-        )
-    }
-
 
     return (
         <>
@@ -35,10 +25,10 @@ export default () => {
             <div className="row m-2 mt-4" style={{justifyContent:"center"}}>
                 <div className="col-3 mb-3">
                     <div className="card" style={{width: "18rem"}}> 
-                        <img src={digimons[0].img} className="card-img-top" alt="pokemon"/>
+                        <img src={digimon[0].img} className="card-img-top" alt="pokemon"/>
                         <div className="card-body">
-                            <h5 className="card-title">{digimons[0].name}</h5>
-                            <p className="card-text">{digimons[0].level}</p>
+                            <h5 className="card-title">{digimon[0].name}</h5>
+                            <p className="card-text">{digimon[0].level}</p>
                             <Link to={`/`} className="btn btn-warning">Back</Link>
                         </div>
                     </div>
