6bb17b7851bd9bd3d52a81e46f859f8a90e0d8c4
mubaraqhere <mubaraqhere@gmail.com>
2020-09-13 07:43:41 +0700

    main page update: + refactor + fixed some bugs

--- client/src/pages/DigimonPage.js
+++ client/src/pages/DigimonPage.js
@@ -1,39 +1,30 @@
-import React from 'react';
-import useFetch from '../hooks/useFetcher'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { getDigimons } from '../store/actions/digimonAction'
 import Card from '../components/Card'
 
 export default () => {
-    const baseUrl = "https://digimon-api.vercel.app/api/digimon"
-    const {data:digimons, loading, error} = useFetch(baseUrl)
 
-    if (loading) {
-        return (
-            <>
-                <div>
-                    <p>Waiting for moment gan....</p>
-                </div>
-            </>
-        )
-    }
+    const data = useSelector((state) => state.digimonReducer.digimons);
+    const [digimons, setDigimons] = useState(data)
+    const dispatch = useDispatch();
 
-    if (error) {
-        return (
-            <>
-                <div>
-                    <p>Error!!!</p>
-                </div>
-            </>
-        )
-    }
+    useEffect(() => {
+        dispatch(getDigimons())
+    }, [dispatch]);
+
+    useEffect(() => {
+        setDigimons(data)
+    }, [data])
 
     return (
         <>
             <div className="mt-2">
-                <h2>Digimon Page</h2>
+                <h2>Digimon List</h2>
                 <div className="mt-4" style={{textAlign:"center"}}>
-                    <form class="form-inline my-2 my-lg-0" style={{display:"inline-block"}}>
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <form className="form-inline my-2 my-lg-0" style={{display:"inline-block"}}>
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search"/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </div>
                 <div className="row m-2 mt-4">
